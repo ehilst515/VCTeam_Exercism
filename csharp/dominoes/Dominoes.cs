0d11ec87a0ce981afa4e2a730da5ae09be86a268
Enrique Hilst <enrique.hilst@gmail.com>
2021-03-31 17:25:24 -0500

    Fixed spelling of domino

--- csharp/dominoes/Dominoes.cs
+++ csharp/dominoes/Dominoes.cs
@@ -14,7 +14,7 @@ namespace DominoesSolution
 
             var firstDom = dominoes.FirstOrDefault();
 
-            // If only one dominoe is entered, return bool for comparison of either side
+            // If only one domino is entered, return bool for comparison of either side
             if (dominoes.Count() == 1)
                 return firstDom.Item1 == firstDom.Item2;
 
@@ -28,42 +28,42 @@ namespace DominoesSolution
         {
             bool result = false;
             // Make a new list
-            List<(int, int)> dominoeChain = new List<(int, int)>();
-            // Add the first dominoe to the list
-            dominoeChain.Add(dom);
-            // Set the first dominoe in the list as current domine
-            var currentDom = dominoeChain.FirstOrDefault();
+            List<(int, int)> dominoChain = new List<(int, int)>();
+            // Add the first domino to the list
+            dominoChain.Add(dom);
+            // Set the first domino in the list as current domino
+            var currentDom = dominoChain.FirstOrDefault();
 
-            foreach (var dominoe in dominoes)
+            foreach (var domino in dominoes)
             {
                 // Skip dominoes already checked
-                if (dominoe == currentDom) continue;
+                if (domino == currentDom) continue;
 
-                var nextDom = dominoe;
-                // Compare current dominoe with next dominoe
+                var nextDom = domino;
+                // Compare current domino with next domino
                 result = Valid(currentDom, nextDom);
 
-                // If result is chainable, add it to the chain and check the next dominoe
+                // If result is chainable, add it to the chain and check the next domino
                 if (result)
                 {
-                    dominoeChain.Add(nextDom);
+                    dominoChain.Add(nextDom);
                     currentDom = nextDom;
                 }
 
-                bool checkedAll = dominoeChain.Count() != dominoes.Count();
+                bool notCheckedAll = dominoChain.Count() != dominoes.Count();
                 // If comparison is not valid and there's more dominoes to check
-                if (!result && checkedAll)
+                if (!result && notCheckedAll)
                 {
                     // Search earlier dominoes for validity
-                    var backtrackResult = BacktrackChain(nextDom, dominoeChain);
+                    var backtrackResult = BacktrackChain(nextDom, dominoChain);
                     result = backtrackResult.Item1;
 
                     // If result is still false, return false
                     if (!result) return false; // isChainable = false
 
-                    // Use new dominoe chain if possible
-                    dominoeChain = backtrackResult.Item2;
-                    // Check the next dominoe
+                    // Use new domino chain if possible
+                    dominoChain = backtrackResult.Item2;
+                    // Check the next domino
                     currentDom = nextDom;
                 }
 
@@ -72,25 +72,25 @@ namespace DominoesSolution
             return result;
         }
 
-        public static System.Tuple<bool, List<(int, int)>> BacktrackChain((int, int) dominoe, List<(int, int)> dominoeChain)
+        public static Tuple<bool, List<(int, int)>> BacktrackChain((int, int) domino, List<(int, int)> dominoChain)
         {
-            for (int i = 1; i < dominoeChain.Count; i++)
+            for (int i = 1; i < dominoChain.Count; i++)
             {
-                var dominoe1 = dominoeChain[i - 1];
-                bool validFrontDominoe = Valid(dominoe1, dominoe);
-                if (validFrontDominoe) // Found one match
+                var domino1 = dominoChain[i - 1];
+                bool validFrontDomino = Valid(domino1, domino);
+                if (validFrontDomino) // Found one match
                 {
-                    var dominoe2 = dominoeChain[i];
-                    bool validBackDominoe = Valid(dominoe2, dominoe);
-                    if (validBackDominoe) // Found adjacent match 
+                    var domino2 = dominoChain[i];
+                    bool validBackDomino = Valid(domino2, domino);
+                    if (validBackDomino) // Found adjacent match 
                     {
-                        dominoeChain.Insert(i, dominoe); // Insert dominoe into list at valid index
-                        return Tuple.Create(true, dominoeChain);
+                        dominoChain.Insert(i, domino); // Insert domino into list at valid index
+                        return Tuple.Create(true, dominoChain);
                     }
                 }
             }
 
-            return Tuple.Create(false, dominoeChain);
+            return Tuple.Create(false, dominoChain);
         }
 
         public static bool Valid((int, int) dom1, (int, int) dom2)
